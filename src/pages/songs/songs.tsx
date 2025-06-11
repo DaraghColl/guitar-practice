8d932a86437703338c9fe689fb992c9d56ffa5b8
Daragh Coll <daraghcoll@gmail.com>
2025-06-11 20:23:07 +0100

    feat:  add sample song to songs page

--- src/pages/songs/songs.tsx
+++ src/pages/songs/songs.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react';
+import chillFunk from '../../assets/songs/chill-funk.mp3';
+import { Play, Square } from 'lucide-react';
+
+const audioElement: HTMLAudioElement = new Audio(chillFunk);
+
 const Songs = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlaySong = () => {
+    if (!isPlaying) {
+      audioElement.play();
+      setIsPlaying(true);
+    } else {
+      audioElement.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      setIsPlaying(false);
+      audioElement.pause();
+    };
+  }, []);
   return (
-    <div>
-      <h1>Songs</h1>
+    <div className="flex h-full flex-col justify-center">
+      <button
+        onClick={togglePlaySong}
+        className="flex h-40 w-full cursor-pointer items-center justify-center rounded-sm bg-linear-to-bl from-gray-500 to-gray-900"
+      >
+        <div className="flex flex-col items-center gap-4">
+          {!isPlaying ? (
+            <Play className="stroke-white" />
+          ) : (
+            <Square className="stroke-white" />
+          )}
+        </div>
+      </button>
     </div>
   );
 };
